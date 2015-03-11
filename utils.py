b9eea5bcc687e4784b46dc913d096bb9e1ab6373
MrLokans <trikster1911@gmail.com>
2015-03-11 19:36:29 +0300

    Added some dummy methods for further extension.

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import os
+import sys
 import json
 import argparse
-import pprint
 from vk_api.Vk import Vk
 
 GROUP_ALIAS = "proglib"
@@ -11,6 +11,7 @@ GROUP_ALIAS = "proglib"
 # - Fix json formatting (spacing).
 # - Make setup.py?
 # - Turn vk_api into dependency
+# - configure argparse
 
 class Post(object):
 
@@ -25,7 +26,6 @@ class Post(object):
 
 
 def get_all_posts(vk, post_num=100):
-    pp = pprint.PrettyPrinter(indent=4)
     posts = []
     offset = 0
     request_num = post_num // 100
@@ -45,10 +45,23 @@ def generate_json_file(posts, filename="my_books.json"):
             json.dump(post, f, indent=4)
 
 
+def setup_argparser(args):
+    parser = argparse.ArgumentParser(description="Option chooser")
+    parser.add_argument('--generate-json', help="An option to generate json.", nargs="?")
+    parser.add_argument('-f', help="Filename data is written to.", action="store_const", const="my_books.json")
+
+    args = parser.parse_args()
+    print(args)
+
+def print_hello():
+    print("Args are working!")
+
+
 def main():
-    vk = Vk()
-    posts = get_all_posts(vk)
-    generate_json_file(posts)
+    setup_argparser(sys.argv)
+    # vk = Vk()
+    # posts = get_all_posts(vk)
+    # generate_json_file(posts)
     # print("Ready to work!")
 
 if __name__ == "__main__":
